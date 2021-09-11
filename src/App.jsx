77c1567f3a33b930bff1bcb762ce66308786563e
saber-mekki <saber.mekki@aureskonnect.com>
2021-09-12 00:25:13 +0100

    feat: add/use 'react google analytics package in 'app' component

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,13 @@ import Education from "./containers/Education";
 import Experience from './containers/Experience';
 import Projects from './containers/Projects';
 import GithubProfile from './containers/GithubProfile'
+import ReactGA from 'react-ga';
+
 const App = () => {
+    React.useEffect(()=>{
+        ReactGA.initialize('UA-207355712-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+    },[])
     return ( 
         <BrowserRouter basename={window.location.pathname || ''}>
             <Switch>
