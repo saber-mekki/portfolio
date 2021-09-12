05d4592f1b3dbfbb7d14091a73a2c0e00fb5b853
saber-mekki <saber.mekki@aureskonnect.com>
2021-09-12 18:51:26 +0100

    format: style

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,44 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Navigation from "./components/Navigation";
 import Greetings from "./containers/Greetings";
 import Skills from "./containers/Skills";
-import Proficiency from './containers/Proficiency';
+import Proficiency from "./containers/Proficiency";
 import Education from "./containers/Education";
-import Experience from './containers/Experience';
-import Projects from './containers/Projects';
-import GithubProfile from './containers/GithubProfile'
-import ReactGA from 'react-ga';
+import Experience from "./containers/Experience";
+import Projects from "./containers/Projects";
+import GithubProfile from "./containers/GithubProfile";
+import ReactGA from "react-ga";
 
 const App = () => {
-    React.useEffect(()=>{
-        ReactGA.initialize('UA-207355712-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
-    },[])
-    return ( 
-        <BrowserRouter basename={window.location.pathname || ''}>
-            <Switch>
-                <Route path="/" exact render={props => (
-                    <>
-                        <Navigation />
-                        <Greetings/>
-                        <Skills />
-                        <Proficiency />
-                        <Education />
-                        <Experience />
-                        <Projects />
-                        <GithubProfile />
-                    </>
-                )}/>
-            </Switch>
-        </BrowserRouter>
-     );
-}
- 
+  React.useEffect(() => {
+    ReactGA.initialize("UA-207355712-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+  
+  return (
+    <BrowserRouter basename={window.location.pathname || ""}>
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={(props) => (
+            <>
+              <Navigation />
+              <Greetings />
+              <Skills />
+              <Proficiency />
+              <Education />
+              <Experience />
+              <Projects />
+              <GithubProfile />
+            </>
+          )}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
 export default App;
